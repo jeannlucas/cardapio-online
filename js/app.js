cb6587958c951a0643d7a1dea1faf8a35ddd7a7d
jeannlucas <jeann.lucas@outlook.com>
2023-11-13 11:04:29 -0300

    animating the layout

--- js/app.js
+++ js/app.js
@@ -549,7 +549,7 @@ cardapio.metodos = {
 
 cardapio.templates = {
   item: `
-        <div class="col-3 mb-5">
+        <div class="col-3 mb-5 animated fadeInUp">
         <div class="card card-item \${id}">
             <div class="img-produto">
                 <img

9d20f4c3ac70ec785bee11e0db701b23c15e8814
jeannlucas <jeann.lucas@outlook.com>
2023-11-13 16:42:54 -0300

    Implemented responsive

--- js/app.js
+++ js/app.js
@@ -549,7 +549,7 @@ cardapio.metodos = {
 
 cardapio.templates = {
   item: `
-        <div class="col-3 mb-5 animated fadeInUp">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 md-5 animated fadeInUp">
         <div class="card card-item \${id}">
             <div class="img-produto">
                 <img
@@ -589,7 +589,7 @@ cardapio.templates = {
                     <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
                     <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
                     <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
-                    <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
+                    <span class="btn btn-remove no-mobile" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
                 </div>
             </div>
                 `,
